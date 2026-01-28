546ad1cc94cd96283510af4da6bde8c6ff76e36b
hhr2449 <hhr24@mails.tsinghua.edu.cn>
2026-01-28 10:58:55 +0800

    patch

--- task2/communicate_primitives/torch_nccl_test.py
+++ task2/communicate_primitives/torch_nccl_test.py
@@ -42,7 +42,7 @@ def test_scatter():
     # 只有进程0创建输入tensor
     if rank == 0:
         # 先创建一个world_size*world_size的一维张量，然后再reshape成world_size行world_size列的二位张量
-        input_tensor = torch.random(world_size * world_size, dtype=torch.float32).reshape(world_size, world_size)
+        input_tensor = torch.arange(world_size * world_size, dtype=torch.float32).reshape(world_size, world_size)
         # 打印输入tensor
         print_rank0(f"Scatter input tensor:\n{input_tensor}")
         # 注意，scatter要求的输入是一个List[Tensor]，而不是一个Tensor
@@ -69,7 +69,7 @@ def test_gather():
     world_size = dist.get_world_size()
     # 创建发送tensor
     # 发送tensor为world_size大小的一维张量
-    send_tensor = torch.random(world_size, dtype=torch.float32)
+    send_tensor = torch.arange(world_size, dtype=torch.float32) + rank * world_size
     # 创建接收tensor
     if rank == 0:
         # 等价于创建一个空的list，然后循环world_size次，每次创建一个张量并且将其append到list中
